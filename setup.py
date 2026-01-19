3b25a44edd1d6956a0cf0728acb8825d9161b311
lsn <lissane2013@gmail.com>
2026-01-19 18:59:14 -0500

    fix gh stars

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="fastmdui",
-    version="0.1.1",
+    version="0.1.0",
     author="Lissan Zahid",
     author_email="lissane2013@duck.com",
     description="Material Design UI components for FastHTML",
